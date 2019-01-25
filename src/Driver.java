8f3674ff66ec34c13de717690ebc3c9c088adf0f
rtritz <rritz@parktudor.org>
2019-01-25 14:43:11 -0500

    Added test to driver

--- src/Driver.java
+++ src/Driver.java
@@ -2,5 +2,6 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Bye");
+        System.out.println("test");
     }
 }
