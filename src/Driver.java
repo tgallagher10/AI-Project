29b1279508450e2ea1efc7c8418829bf14058cec
tgallagher10 <tgallagher@parktudor.org>
2019-01-31 12:36:23 -0500

    added game menu

--- src/Driver.java
+++ src/Driver.java
@@ -4,8 +4,16 @@ public class Driver {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.println("1. Tic-Tac-Toe (Human vs Human");
+        System.out.println("2. Tic-Tac-Toe (Human vs Beginner AI");
+        System.out.println("3. Tic-Tac-Toe (Human vs Pro AI");
+        System.out.println("4. Connect 4 (Human vs Human");
+        System.out.println("5. Connect 4 (Human vs Beginner AI");
+        System.out.println("6. Connect 4 (Human vs Pro AI");
+        System.out.println("Select your game (1-6):");
+
         Board board = null;
-        if(in.next().equals("TTT")){
+        if(in.next().equals("1")){
             board = new TTTBoard();
     }
 
