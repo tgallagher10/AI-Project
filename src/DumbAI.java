7bd092137b668168d7c105dfb9424b73836a53f3
tgallagher10 <tgallagher@parktudor.org>
2019-02-04 09:25:35 -0500

    added diaggap method and checked for win in getMove method

--- src/DumbAI.java
+++ src/DumbAI.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class DumbAI extends Player {
 
 
@@ -5,18 +7,37 @@ public class DumbAI extends Player {
         super (inName);
     }
     public String getMove(Board board){
-        
+        ArrayList<String> spots=new ArrayList<>();
+        spots=board.getEmptyLocs();
+        for (int i=0; i<spots.size(); i++){
+            board.placePiece(spots.get(i), "X");
+            if (board.isWinner("X")){
+                return spots.get(i);
+            }
+            else{
+                board.retractPiece(spots.get(i));
+            }
+        }
     }
 
 
 
+    private ArrayList<Integer> isColgap (Board board){}
 
 
-    private boolean isColgap (Board board){}
+    private ArrayList<Integer> isRowgap(Board board){}
 
 
-    private boolean isRowgap( Board board){}
+    private ArrayList<Integer> isDiaggap(Board board){
+        ArrayList<Integer> location=new ArrayList<>();
+        if (board.get(0,0).equals(board.get(2,2)) && board.getEmptyLocs().contains("5")){
+            location.add(5);
+        }
 
+        else if (board.get(0,2).equals(board.get(2,0)) && board.getEmptyLocs().contains("5")){
+            location.add(5);
+        }
 
-    private boolean isDiaggap(Board board){}
+        return location;
+    }
 }
